fb25c31a3c9fffab889312c5967546150b39f7f8
rodribro <rodrigo.castro.pt@gmail.com>
2026-02-14 22:07:35 +0000

    Dummy detection service

---
+++ src/shelfsight/services/detection.py
@@ -0,0 +1,11 @@
+class DetectionService:
+    def __init__(self, model):
+        self.model = model
+
+    def create_run(self, image):
+        # Placeholder for creating a new run in the database
+        run_id = 12345
+        detections = {}
+        total_detections = 0
+        products = {}
+        return run_id, detections, total_detections, products
