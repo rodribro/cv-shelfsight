0cd88f037c4e1187f8a17833cfb1c26616d7b243
rodribro <rodrigo.castro.pt@gmail.com>
2026-02-16 10:46:41 +0000

    Added routing for detection endpoint

--- src/shelfsight/api/router.py
+++ src/shelfsight/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from shelfsight.api.v1.routers import health
+from shelfsight.api.v1.routers import detections, health
 
 api_router = APIRouter()
 api_router.include_router(health.router)
+api_router.include_router(detections.router)
